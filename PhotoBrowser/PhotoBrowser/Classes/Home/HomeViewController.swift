c6cc332554592283a949888fa464e2202c40b34b
huangxiaoer <434891335@qq.com>
2016-04-28 09:56:45 +0800

    初始化项目

---
+++ PhotoBrowser/PhotoBrowser/Classes/Home/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  PhotoBrowser
+//
+//  Created by 李先生 on 16/4/28.
+//  Copyright © 2016年 MaYuan. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class HomeViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
